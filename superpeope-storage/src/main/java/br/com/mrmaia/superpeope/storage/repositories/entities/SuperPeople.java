ae7acd85c6377292c20eb421f6efa0ba646b10aa
Silvio Maia <ingles.silvio@gmail.com>
2023-05-03 17:08:13 -0300

    created SuperPeople entity and also SuperPower entity in order to satisfy superPowers attribute in SuperPeople

--- superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/repositories/entities/SuperPeople.java
+++ superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/repositories/entities/SuperPeople.java
@@ -1,4 +1,51 @@
-package br.com.mrmaia.superpeope.storage.repositories;
+package br.com.mrmaia.superpeope.storage.repositories.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "TB_SUPERPEOPLE")
+@SequenceGenerator(name = "SEQ_SUPERPEOPLE")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class SuperPeople {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SUPERPEOPLE")
+    @Column(name = "ID_SUPERPEOPLE")
+    private Long id;
+
+    @Column(name = "DS_NAME")
+    private String name;
+
+    @Column(name = "DS_PLANET")
+    private String planet;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "DS_SUPERPOWERS")
+    private List<SuperPower> superPowers;
+
+    @Column(name = "DS_LEVEL")
+    private Long level;
+
+    @Column(name = "DS_CURRENTEXPERIENCE")
+    private Long currentExperience;
+
+    @Column(name = "DS_NEXTLEVELEXPERIENCE")
+    private Long nextLevelExperience;
+
+    @Column(name = "DS_TYPE")
+    private String type;
+
+    public Long getId() { return id; }
+
+    public void setId(long id) { this.id = id; }
 }
---
+++ superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/repositories/entities/SuperPower.java
@@ -0,0 +1,29 @@
+package br.com.mrmaia.superpeope.storage.repositories.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_SUPERPOWER")
+@SequenceGenerator(name = "SEQ_SUPERPOWER")
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuperPower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SUPERPOWER")
+    @Column(name = "ID_SUPERPOWER")
+    private Long id;
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+
+}
