f1a6ac9c85c30e9f6aed12a8b27c895a483c5b11
Silvio Maia <ingles.silvio@gmail.com>
2023-08-09 18:46:35 -0300

    finished opponentGenerator and listAdder classes

--- superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/services/impl/BattleService.java
+++ superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/services/impl/BattleService.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static java.util.Collections.addAll;
+
 @Service
 @Slf4j
 public class BattleService implements IBattleService {
@@ -30,7 +32,7 @@ public class BattleService implements IBattleService {
         List<SuperPeople> foundOpponents = new ArrayList<>();
         long opponentAmount = 0;
         for (SuperPeople opponent : opponentList) {
-            if (opponent.getLevel() == level && opponentAmount <= desiredOpponentAmount) {
+            if (opponent.getLevel().equals(level) && opponentAmount <= desiredOpponentAmount) {
                 foundOpponents.add(opponent);
                 opponentAmount++;
             }
@@ -39,28 +41,46 @@ public class BattleService implements IBattleService {
     }
 
     private List<SuperPeople> opponentGenerator(List<SuperPeople> foundOpponents, SuperPeople superPerson,
-                                                Long foundOpponentAmount, Long desiredOpponentAmount) {
-        if (foundOpponentAmount == null) {
+                                                Long desiredOpponentAmount) {
+        int instance = 0;
+        if (foundOpponents == null) {
             List<SuperPeople> generatedOpponents = new ArrayList<>();
-            int instance = 0;
-
             do {
                 Random random = new Random();
                 int range = 1;
-
                 SuperPeople generatedOpponent = SuperPeople.builder()
                         .strength(superPerson.getStrength() - range + random.nextInt(2 * range + 1))
                         .dexterity(superPerson.getDexterity()-range+random.nextInt(2*range+1))
                         .level(superPerson.getLevel()-range+random.nextInt(2*range+1))
                         .constitution(superPerson.getConstitution()-range+ random.nextInt(2*range+1))
                         .build();
-
                 generatedOpponents.add(generatedOpponent);
-
                 instance++;
             } while (instance <= desiredOpponentAmount);
-
             return generatedOpponents;
+
+        } else if (foundOpponents.size() < desiredOpponentAmount) {
+            List<SuperPeople> generatedOpponents = new ArrayList<>();
+            do {
+                Random random = new Random();
+                int range = 1;
+                SuperPeople generatedOpponent = SuperPeople.builder()
+                        .strength(superPerson.getStrength() - range + random.nextInt(2 * range + 1))
+                        .dexterity(superPerson.getDexterity()-range+random.nextInt(2*range+1))
+                        .level(superPerson.getLevel()-range+random.nextInt(2*range+1))
+                        .constitution(superPerson.getConstitution()-range+ random.nextInt(2*range+1))
+                        .build();
+                generatedOpponents.add(generatedOpponent);
+                instance++;
+            } while (instance <= (desiredOpponentAmount-foundOpponents.size()));
+            return generatedOpponents;
+
         }
+        return null;
+    }
+
+    private List<SuperPeople> listAdder (List<SuperPeople> finalList, List<SuperPeople> listToAdd) {
+        finalList.addAll(listToAdd);
+        return finalList;
     }
 }
