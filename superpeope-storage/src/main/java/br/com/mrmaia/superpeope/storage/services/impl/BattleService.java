7ed941e19f3fd437b8a86bbd7a4b6347becc0d3c
Silvio Maia <ingles.silvio@gmail.com>
2023-08-09 18:06:07 -0300

    did first part of opponentGenerator method

--- superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/services/impl/BattleService.java
+++ superpeope-storage/src/main/java/br/com/mrmaia/superpeope/storage/services/impl/BattleService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 @Slf4j
@@ -25,15 +26,41 @@ public class BattleService implements IBattleService {
     }
 
     private List<SuperPeople> opponentFinder(List<SuperPeople> opponentList, Long level,
-                                             Long totalOpponentAmount) {
+                                             Long desiredOpponentAmount) {
         List<SuperPeople> foundOpponents = new ArrayList<>();
         long opponentAmount = 0;
         for (SuperPeople opponent : opponentList) {
-            if (opponent.getLevel() == level && opponentAmount < totalOpponentAmount) {
+            if (opponent.getLevel() == level && opponentAmount <= desiredOpponentAmount) {
                 foundOpponents.add(opponent);
                 opponentAmount++;
             }
         }
         return foundOpponents;
+    }
+
+    private List<SuperPeople> opponentGenerator(List<SuperPeople> foundOpponents, SuperPeople superPerson,
+                                                Long foundOpponentAmount, Long desiredOpponentAmount) {
+        if (foundOpponentAmount == null) {
+            List<SuperPeople> generatedOpponents = new ArrayList<>();
+            int instance = 0;
+
+            do {
+                Random random = new Random();
+                int range = 1;
+
+                SuperPeople generatedOpponent = SuperPeople.builder()
+                        .strength(superPerson.getStrength() - range + random.nextInt(2 * range + 1))
+                        .dexterity(superPerson.getDexterity()-range+random.nextInt(2*range+1))
+                        .level(superPerson.getLevel()-range+random.nextInt(2*range+1))
+                        .constitution(superPerson.getConstitution()-range+ random.nextInt(2*range+1))
+                        .build();
+
+                generatedOpponents.add(generatedOpponent);
+
+                instance++;
+            } while (instance <= desiredOpponentAmount);
+
+            return generatedOpponents;
+        }
     }
 }
